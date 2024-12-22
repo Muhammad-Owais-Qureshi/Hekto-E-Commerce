d678ad671516ace09df6662d04eff12cc6f21e8d
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2024-12-23 04:11:02 +0500

    page

---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Hero from './components/hero'
+import FeaturedProducts from './components/FeaturedProducts'
+import FeaturedHero from './components/FeaturedHero'
+import Latest from './components/latest'
+import LatestHero from './components/latestHero'
+import LatexstHero2 from './components/latexstHero2'
+import Heading from './components/heading'
+import Service from './components/service'
+import Hero2 from './components/hero2'
+import TrendingPro from './components/trendingPro'
+import ThreePortion from './components/threePortion'
+import DiscountHero from './components/discountHero'
+import TopCet from './components/topCet'
+import NewsShelter from './components/newsShelter'
+import Logo from './components/logo'
+import Blog from './components/blog'
+const page = () => {
+  return (
+    <div>
+      <Hero/>
+      <FeaturedProducts/>
+      <FeaturedHero/>
+      <Latest/>
+      <LatestHero/>
+      <LatexstHero2/>
+      <Heading/>
+      <Service/>
+      <Hero2/>
+      <TrendingPro/>
+      <ThreePortion/>
+      <DiscountHero/>
+      <TopCet/>
+      <NewsShelter/>
+      <Logo/>
+      <Blog/>
+    </div>
+  )
+}
+
+export default page
