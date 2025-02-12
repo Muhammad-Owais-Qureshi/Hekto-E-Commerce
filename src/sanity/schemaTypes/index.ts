9b69bef19c887c05dbaa84b0d84d90943c657b04
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2025-02-13 03:46:13 +0500

    check

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import product from '../../../next-ecommerce-template-4/src/sanity/schemaTypes/product'
+import product from './product'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [product],
